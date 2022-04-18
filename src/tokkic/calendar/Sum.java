0dd15e7979e82723e9576d884f57aa1f70ee46b9
2ukite <53216523+2ukite@users.noreply.github.com>
2022-04-18 16:03:00 +0900

    nextInt 로 입력처리, printf 사용

--- src/tokkic/calendar/Sum.java
+++ src/tokkic/calendar/Sum.java
@@ -4,24 +4,21 @@ import java.util.Scanner;
 
 public class Sum {
 	public static void main(String[] args) {
-		int a, b;
-		
+//		int a, b;
+
 		// 키보드로 두 수의 입력 받기
 		Scanner scan = new Scanner(System.in);
-		String s1, s2;
+		int s1, s2;
 		System.out.println("두 수를 입력해주세요");
-		s1 = scan.next();
-		s2 = scan.next();
-		a = Integer.parseInt(s1);
-		b = Integer.parseInt(s2);
-		
-//		System.out.println(a);
-//		System.out.println(b);				
-				
-		//  화면에 두 수의 합을 출력하기
-		System.out.printf("%d와 %d의 합은 %d 입니다",a,b,a+b);
-		
+		s1 = scan.nextInt();
+		s2 = scan.nextInt();
+
+//		System.out.println(s1);
+//		System.out.println(s2);
+
+//		//  화면에 두 수의 합을 출력하기
+		System.out.printf("%d와 %d의 합은 %d 입니다", s1, s2, s1 + s2);
 		scan.close();
 		
-        	}
+	}
 }
