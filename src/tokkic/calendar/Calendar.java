a7a58123bc870946f5612086c8bb912ba069aeca
2ukite <53216523+2ukite@users.noreply.github.com>
2022-04-25 17:12:38 +0900

    while 반복문에서의 if-break, if-continue

--- src/tokkic/calendar/Calendar.java
+++ src/tokkic/calendar/Calendar.java
@@ -3,6 +3,13 @@ package tokkic.calendar;
 import java.util.Scanner;
 
 public class Calendar {
+
+	private static final int[] MAX_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+	public int getMaxDaysofMMonth(int month) {
+		return MAX_DAYS[month - 1];
+	}
+
 	public static void main(String[] args) {
 
 		System.out.println("일 월 화 수 목 금 토");
@@ -10,39 +17,34 @@ public class Calendar {
 		System.out.println(" 1  2  3  4  5  6  7");
 		System.out.println(" 8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
-		System.out.println("22 23 24 25 26 27 28");
+		System.out.println("22 23 24 25 26 27 28\r\n");
 
-		// 반복횟수 입력
-
-		System.out.println("반복횟수를 입력하세요.");
+		// // 반복횟수 입력 삭제, 입력 시 월 별 최대 일 바로 출력, 입력 무한반복
+		// // 배열 안쓰기, break 와 continue 사용하여 조건문 만들기
 
 		Scanner scan = new Scanner(System.in);
-		int month_retry_num = scan.nextInt();
+		Calendar cld = new Calendar();
+		String prompt = "cal> ";
 
+		while(true) {
+			
 		System.out.println("월을 입력하세요.");
 
-		// 입력된 반복 횟수 만큼 입력하여 배열에 저장
-
-		int[] month_data = new int[month_retry_num];
+		System.out.print(prompt);
 		
-		for (int month_retry = 0; month_retry < month_retry_num; month_retry++) {
-
-			month_data[month_retry] = scan.nextInt();
-			
+		int month = scan.nextInt();
+		
+		if(month<=-1) {
+			break;
 		}
 
-		// 배열의 데이터 값을 꺼내어 출력하기
-
-		byte month_day[] = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-		
-		for (int i = 0; i < month_data.length; i++) {
-			
-			int m1 = month_data[i];
-			int m2 = month_day[m1 - 1];
-			
-			System.out.printf("%d월은 %d일까지 있습니다.", m1, m2);
-			
+		if(month>=13) {
+			continue;
 		}
+		System.out.printf("%d월은 %d일까지 있습니다.\r\n", month, cld.getMaxDaysofMMonth(month));
+
+		}
+		System.out.println("Have a nice day!");
 		scan.close();
 	}
 }
