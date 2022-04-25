13b6b68ca29609d706d58a0404923b3ac22ae009
2ukite <53216523+2ukite@users.noreply.github.com>
2022-04-25 17:26:05 +0900

    최대 일수 수정, 그에 따른 달력 수정

--- src/tokkic/calendar/Calendar.java
+++ src/tokkic/calendar/Calendar.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Calendar {
 
-	private static final int[] MAX_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
 	public int getMaxDaysofMMonth(int month) {
 		return MAX_DAYS[month - 1];
@@ -17,7 +17,7 @@ public class Calendar {
 		System.out.println(" 1  2  3  4  5  6  7");
 		System.out.println(" 8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
-		System.out.println("22 23 24 25 26 27 28\r\n");
+		System.out.println("22 23 24 25 26 27 28");
 
 		// // 반복횟수 입력 삭제, 입력 시 월 별 최대 일 바로 출력, 입력 무한반복
 		// // 배열 안쓰기, break 와 continue 사용하여 조건문 만들기
