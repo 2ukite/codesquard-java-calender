e2e17be957dec4625204829572edd6ad0259d521
2ukite <53216523+2ukite@users.noreply.github.com>
2022-04-25 21:58:53 +0900

    ?

--- src/tokkic/calendar/Sum.java
+++ src/tokkic/calendar/Sum.java
@@ -5,18 +5,28 @@ import java.util.Scanner;
 public class Sum {
 	public static void main(String[] args) {
 
-		System.out.println("달을 입력하세요");
-
-		// 키보드로 입력 받기
-		Scanner scan = new Scanner(System.in);
-		byte s1, s2;
-		byte month_day[] = {31, 29, 31, 30, 31, 30 ,31, 31, 30, 31, 30, 31};
-		s1 = scan.nextByte();
-		s2 = month_day[s1-1];
-
-		//  화면에 두 수의 합을 출력하기
-		System.out.printf("%d월은 %d일까지 있습니다.",s1, s2);
-		scan.close();
+//		System.out.println("달을 입력하세요");
+//
+//		// 키보드로 입력 받기
+//		Scanner scan = new Scanner(System.in);
+//		byte s1, s2;
+//		byte month_day[] = {31, 29, 31, 30, 31, 30 ,31, 31, 30, 31, 30, 31};
+//		s1 = scan.nextByte();
+//		s2 = month_day[s1-1];
+//
+//		//  화면에 두 수의 합을 출력하기
+//		System.out.printf("%d월은 %d일까지 있습니다.",s1, s2);
+//		scan.close();
+//		
+		
+//		double doubleNum = 1.12345;
+		float floatNum = 1.234f;
+		
+		//double to int
+	int	intNum = (int)floatNum;
+	float re = (float)intNum;
+		
+		System.out.println(re);
 		
 	}
 }
