bfd1c496e293f4783173a44f600051d193cbf621
2ukite <53216523+2ukite@users.noreply.github.com>
2022-04-28 03:42:12 +0900

    불필요한 import 제거

--- src/tokkic/calendar/Calendar.java
+++ src/tokkic/calendar/Calendar.java
@@ -1,7 +1,5 @@
 package tokkic.calendar;
 
-import javax.swing.text.html.parser.ParserDelegator;
-
 public class Calendar {
 
 	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
@@ -17,34 +15,27 @@ public class Calendar {
 	public void printCalendar(int year, int month, String weekday) {
 		int blankDay;
 		int wDay_num;
-		
+
 		Prompt prom = new Prompt();
-		wDay_num = prom.parseDay(weekday);	
-		blankDay = 7-wDay_num;
+		wDay_num = prom.parseDay(weekday);
+		blankDay = 7 - wDay_num;
 		int maxDay = getMaxDaysofMMonth(year, month);
-		
+
 		System.out.printf("    <<%4d년 % 3d월>>\n", year, month);
 		System.out.println(" SU MO TU WE TH FR SA");
 		System.out.println("---------------------");
 
-		for (int i = 0; i<wDay_num; i++) {
+		for (int i = 0; i < wDay_num; i++) {
 			System.out.print("   ");
 		}
-		
+
 		for (int i = 1; i <= maxDay; i++) {
 			System.out.printf("%3d", i);
 			if (i % 7 == blankDay) {
 				System.out.println();
 			}
 		}
-		
-		
-		
 
 		System.out.println();
-//		System.out.println(" 1  2  3  4  5  6  7");
-//		System.out.println(" 8  9 10 11 12 13 14");
-//		System.out.println("15 16 17 18 19 20 21");
-//		System.out.println("22 23 24 25 26 27 28");
 	}
 }
